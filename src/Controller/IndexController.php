c84e7761a07faf093102b3f3f670d07c63f729ae
Balazs Csor <balazscsor@gmail.com>
2023-07-10 14:17:24 +0200

    CRUD of contacts

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index() {
+
+    }
+}
